30e9f73d57872884c5aa18b6f8b574e905bbdff6
Matheus <matheusvilar8@gmail.com>
2020-09-20 22:55:38 -0300

    Adicionar arquivos de projeto.

---
+++ Model/MongoDBModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartPorteAPI.Model
+{
+    public class MongoDBModel
+    {
+        public string recvTime { get; set; }
+        public string attrName { get; set; }
+        public string attrType { get; set; }
+        public string attrValue { get; set; }
+    }
+}
---
+++ Model/PosteModel.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartPorteAPI.Model
+{
+    public class PosteModel
+    {
+        [BsonId]
+        public string Id { get; set; }
+        [BsonElement("vazao")]
+        public double Vazao { get; set; }
+        [BsonElement("temperature")]
+        public double Temperatura { get; set; }
+        [BsonElement("luz")]
+        public double Luminosidade { get; set; }
+        [BsonElement("energia")]
+        public double Energia { get; set; }
+
+        public string recvTime { get; set; }
+        public string attrName { get; set; }
+        public string attrType { get; set; }
+        public string attrValue { get; set; }
+    }
+}
